39830653b3706450b58020e7a9a0fc7ef69576e6
MaexBower <max@maexbower.de>
2017-04-25 19:11:46 +0200

    added some tests

--- 03/src/main.cpp
+++ 03/src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "../include/PreAllocString.h"
 int main() {
-    CREATE(str25, 25);
+   CREATE(str25, 25);
     printf("Size: %u\n", static_cast<unsigned int>(str25.SizeOf()));
     const char *cstr = "fester string";
     printf("String: %s\n", str25);
@@ -42,4 +42,74 @@ int main() {
     printf("Length: %u\n", static_cast<unsigned int>(str1.GetLength()));
     return 0;
 
+/*
+    CREATE(test_string_5,5);
+    CREATE(test_string_20,20);
+    CREATE(test_string_100,100);
+    int counter=1;
+
+    printf("-->Test %d. Overflow:\n",counter++);
+    */
+/* Test overflow *//*
+
+    test_string_5 = "abcdefghijk";
+    printf("\t\tInhalt: %s\t Länge: %d\t Maximum: %d\n",\
+            (const char*)test_string_5,\
+            test_string_5.GetLength(),\
+            test_string_5.SizeOf());
+    printf("-->Test %d. char operators:\n",counter++);
+    */
+/* Test operator = for char *//*
+
+    test_string_20 = 'a';
+    test_string_20.AddWhiteSpace();
+    */
+/* Test operator += for char *//*
+
+    test_string_20 += 'b';
+    printf("\t\tInhalt: %s\t Länge: %d\t Maximum: %d\n",
+           (const char*)test_string_20,
+           test_string_20.GetLength(),
+           test_string_20.SizeOf());
+    printf("-->Test %d. String operators:\n",counter++);
+    */
+/* Test operator = for char array*//*
+
+    test_string_100 = "abcdef";
+    test_string_100.AddWhiteSpace();
+    */
+/* Test operator += for char array*//*
+
+    test_string_100 += "ghijklo";
+    printf("\t\tInhalt: %s\t Länge: %d\t Maximum: %d\n",
+           (const char*)test_string_100,
+           test_string_100.GetLength(),
+           test_string_100.SizeOf());
+    printf("-->Test %d. Array operator\n\t\tarray[%d]\t=\tValue: %c\n\t\tarray[%d]\t=\tValue: %c\n",
+           counter++,
+           1,
+           test_string_100[1],
+           101,
+           test_string_100[101]);
+    test_string_100[1];
+    printf("\t\tInhalt: %s\t Länge: %d\t Maximum: %d\n",\
+            (const char*)test_string_100,\
+            test_string_100.GetLength(),\
+            test_string_100.SizeOf());
+    printf("-->Test %d. indirekt test of AddFormat and Empty:\n",counter++);
+    test_string_100="abcdefghijklmnopqrstuvwxyz";
+    printf("\t\tInhalt: %s\t Länge: %d\t Maximum: %d\n",\
+            (const char*)test_string_100,\
+            test_string_100.GetLength(),\
+            test_string_100.SizeOf());
+
+    //CREATE(test_string20_2,20);
+    */
+/*printf("-->Test %d. Copy Class:\n",counter++);
+     *test_string20_2="zyxw";
+     * fails-->> test_string_20=test_string20_2;
+     *//*
+
+
+    printf("--> Test ENDE \n");
 }
